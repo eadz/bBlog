2190be38c02d9c2f599c4e7abf8787e64aa9663d
telcor <telcor@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-08-02 03:44:05 +0000

    The simple post modifier now uses the transformLinks method of the StringHandling class. This is to solve Flyspray #49
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@28 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/bBlog_plugins/modifier.simple.php
+++ bblog/bBlog_plugins/modifier.simple.php
@@ -25,34 +25,20 @@ function identify_modifier_simple () {
     'description'   =>'Converts breaks to newlines and URLs into clickable links',
     'authors'        =>'Eaden McKee, phpBB Authors',
     'licence'         =>'GPL',
-    'help'    	=> 'This is a simple modifier that simply converts new lines ( returns ) into html breaks, any urls ( e.g. http://www.bblog.com/ or www.bblog.com into clickable links.'
+    'help'    	=> 'This is a simple modifier that simply converts new lines ( returns ) into html breaks, any urls ( e.g. http://www.bblog.com/ or www.bblog.com) into clickable links.'
   );
 }
 ////
 // !a simple modifier combining nl2br and make clickable
-function smarty_modifier_simple ($ret) {
-                // pad it with a space so we can match things at the start of the 1st line.
-	$ret = ' ' . $ret;
-
-	// matches an "xxxx://yyyy" URL at the start of a line, or after a space.
-	// xxxx can only be alpha characters.
-	// yyyy is anything up to the first space, newline, comma, double quote or <
-	$ret = preg_replace("#([\t\r\n ])([a-z0-9]+?){1}://([\w\-]+\.([\w\-]+\.)*[\w]+(:[0-9]+)?(/[^ \"\n\r\t<]*)?)#i", '\1<a href="\2://\3" target="_blank">\2://\3</a>', $ret);
-
-	// matches a "www|ftp.xxxx.yyyy[/zzzz]" kinda lazy URL thing
-	// Must contain at least 2 dots. xxxx contains either alphanum, or "-"
-	// zzzz is optional.. will contain everything up to the first space, newline,
-	// comma, double quote or <.
-	$ret = preg_replace("#([\t\r\n ])(www|ftp)\.(([\w\-]+\.)*[\w]+(:[0-9]+)?(/[^ \"\n\r\t<]*)?)#i", '\1<a href="http://\2.\3" target="_blank">\2.\3</a>', $ret);
-
-	// matches an email@domain type address at the start of a line, or after a space.
-	// Note: Only the followed chars are valid; alphanums, "-", "_" and or ".".
-	$ret = preg_replace("#([\n ])([a-z0-9\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
-
-	// Remove our padding..
-	$ret = substr($ret, 1);
-
-     return nl2br($ret);
+function smarty_modifier_simple ($body) {
+    //Replaced all code with methods in StringHandler class
+    
+    $parts = explode(" ", $body);
+    foreach($parts as $ind=>$line){
+        $parts[$ind] = StringHandling::transformLinks($line);
+    }
+    $body = join(" ", $parts);
+    return nl2br($body);
 }
 
 

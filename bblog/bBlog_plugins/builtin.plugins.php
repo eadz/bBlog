22617039f26a4ee51079afdc2009d306ac82493d
tobymiller <tobymiller@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-08-04 02:38:55 +0000

    if no plugins are returned from the database then $plugins_from_db is empty and running a foreach on it creates an invalid argument error. I added a safety check for $plugins_from_db before looping it.
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@47 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/bBlog_plugins/builtin.plugins.php
+++ bblog/bBlog_plugins/builtin.plugins.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/copyleft/gpl.html GPL
  * @package bblog
  */
- 
+
 // this is supposed to be a GUI for plugins.
 // need some help with some sort of api.
 // in the mean time we'll put them in an array.
@@ -23,7 +23,7 @@ function identify_admin_plugins () {
     'licence'         =>'GPL'
   );
 }
-// PLUGINS : 
+// PLUGINS :
 
 function scan_for_plugins () {
   global $bBlog;
@@ -33,7 +33,7 @@ function scan_for_plugins () {
   $plugins = $bBlog->get_results("select * from ".T_PLUGINS);
   if(is_array($plugins)) foreach($plugins as $plugin)
                            $have_plugins[$plugin->type][$plugin->name] = TRUE;
-            
+
   $plugin_files=array();
   $dir="./bBlog_plugins";
   $dh = opendir( $dir ) or die("couldn't open directory");
@@ -51,18 +51,18 @@ function scan_for_plugins () {
        if(function_exists($func)) {
                 $newplugin = $func();
                 if($have_plugins[$newplugin['type']][$newplugin['name']]!==TRUE) {
-			 $q = "insert into ".T_PLUGINS." set
+             $q = "insert into ".T_PLUGINS." set
                          `type`='".$newplugin['type']."',
                         `name`='".$newplugin['name']."',
                          nicename='".$newplugin['nicename']."',
                          description='".addslashes($newplugin['description'])."',
-			 template='".$newplugin['template']."',
+             template='".$newplugin['template']."',
                          help='".addslashes($newplugin['help'])."',
                          authors='".addslashes($newplugin['authors'])."',
                          licence='".$newplugin['licence']."'";
-			 $bBlog->query($q);
-			 $newplugincount++;
-			 $newpluginnames[]=$newplugin['nicename'];
+             $bBlog->query($q);
+             $newplugincount++;
+             $newpluginnames[]=$newplugin['nicename'];
 
                 }
        }
@@ -76,29 +76,33 @@ function scan_for_plugins () {
 if(isset($_POST['scan'])) $np = scan_for_plugins();
 
 if(isset($_POST['scan_refresh'])) {
-	$bBlog->query("delete  from ".T_PLUGINS);
-	$np = scan_for_plugins();
-	$bBlog->smartyObj->assign('np',"<b style='color: red;'>$np</b><br />");
+    $bBlog->query("delete  from ".T_PLUGINS);
+    $np = scan_for_plugins();
+    $bBlog->smartyObj->assign('np',"<b style='color: red;'>$np</b><br />");
 }
 
 
 
 
 $plugins_from_db = $bBlog->get_results("select * from ".T_PLUGINS." order by type");
-foreach($plugins_from_db as $plugin_from_db) {
-   $plugins[$plugin_from_db->type][$plugin_from_db->name]= array( 
-	"name"=>$plugin_from_db->name,
-	"id" => $plugin_from_db->id,
-	"type"=>$plugin_from_db->type,
-	"displayname"=>$plugin_from_db->nicename,
-	"description"=>$plugin_from_db->description,
-	"file"=>$plugin_from_db->type.".".$plugin_from_db->name.".php",
-	"template"=>$plugin_from_db->template,
-	"author"=>$plugin_from_db->authors,
-	"licence"=>$plugin_from_db->licence,
-	"help"=>$plugin_from_db->help
-	);
-   $plugin_ar[] = $plugins[$plugin_from_db->type][$plugin_from_db->name];
+if ($plugins_from_db)
+{
+    foreach($plugins_from_db as $plugin_from_db)
+    {
+        $plugins[$plugin_from_db->type][$plugin_from_db->name]= array(
+        "name"=>$plugin_from_db->name,
+        "id" => $plugin_from_db->id,
+        "type"=>$plugin_from_db->type,
+        "displayname"=>$plugin_from_db->nicename,
+        "description"=>$plugin_from_db->description,
+        "file"=>$plugin_from_db->type.".".$plugin_from_db->name.".php",
+        "template"=>$plugin_from_db->template,
+        "author"=>$plugin_from_db->authors,
+        "licence"=>$plugin_from_db->licence,
+        "help"=>$plugin_from_db->help
+        );
+        $plugin_ar[] = $plugins[$plugin_from_db->type][$plugin_from_db->name];
+    }
 }
 // other plugin :
 
@@ -110,11 +114,11 @@ if($_GET['p']) $p = $_GET['p'];
 if($_POST['p']) $p = $_POST['p'];
 
 if($p && is_array($plugins['admin'][$p])) { // successful call to plugin
-	$show_plugin_menu = FALSE;
-	$bBlog->smartyObj->assign('plugin',$plugins['admin'][$p]);
-	$bBlog->smartyObj->assign('plugin_template','plugins/'.$plugins['admin'][$p]['template']);
-	$bBlog->smartyObj->assign('title',$plugins['admin'][$p]['displayname']);
-	include_once BBLOGROOT.'bBlog_plugins/'.$plugins['admin'][$p]['file'];
+    $show_plugin_menu = FALSE;
+    $bBlog->smartyObj->assign('plugin',$plugins['admin'][$p]);
+    $bBlog->smartyObj->assign('plugin_template','plugins/'.$plugins['admin'][$p]['template']);
+    $bBlog->smartyObj->assign('title',$plugins['admin'][$p]['displayname']);
+    include_once BBLOGROOT.'bBlog_plugins/'.$plugins['admin'][$p]['file'];
         $func = "admin_plugin_".$p."_run";
         $func($bBlog);
 }

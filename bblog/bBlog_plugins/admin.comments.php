f07dc35fe1d49ce5b79364386aa0c83b3ffafff8
tobymiller <tobymiller@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-08-04 21:04:21 +0000

    updated 0.7.6 reference from $bBlog->assign to $bBlog->smartyObj->assign
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@59 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/bBlog_plugins/admin.comments.php
+++ bblog/bBlog_plugins/admin.comments.php
@@ -1,11 +1,23 @@
 <?php
-/**
- * admin.comments.php - administer comments
- * <p>
- * @copyright Copyright (C) 2003  Eaden McKee <email@eadz.co.nz>
- * @license http://www.gnu.org/copyleft/gpl.html GPL
- * @package bblog
- */
+// admin.comments.php - administer comments
+/*
+** bBlog Weblog http://www.bblog.com/
+** Copyright (C) 2003  Eaden McKee <email@eadz.co.nz>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 function identify_admin_comments () {
     return array (
@@ -32,7 +44,7 @@ function admin_plugin_comments_run(&$bBlog) {
     else{
         $commentdo = "";
     }
-    
+
     switch($commentdo) {
         case "Delete" : // delete comments
             if(is_array($_POST['commentid'])){
@@ -65,10 +77,10 @@ function admin_plugin_comments_run(&$bBlog) {
         default : // show form
             break;
     }
-    
+
     retrieveComments(&$bBlog, $commentAmount);
     populateSelectList(&$bBlog);
-    
+
 }
 
 function deleteComment(&$bBlog, $id){
@@ -94,8 +106,8 @@ function editComment(&$bBlog, $commentid, $postid){
         if(!$comment)
             $rval = false;
         if($rval === true){
-            $bBlog->assign('showeditform',TRUE);
-            $bBlog->assign('comment',$comment[0]);
+            $bBlog->smartyObj->assign('showeditform',TRUE);
+            $bBlog->smartyObj->assign('comment',$comment[0]);
         }
     }
     return $rval;
@@ -115,7 +127,7 @@ function saveEdit(&$bBlog){
         if($rval === true){
             $q = "update ".T_COMMENTS." set title='$title', postername='$author', posterwebsite='$websiteurl', posteremail='$email', commenttext='$body' where commentid='{$_POST['commentid']}'";
             if($bBlog->query($q) === true)
-                $bBlog->assign('message', 'Comment <em>'.$title.'</em> saved');
+                $bBlog->smartyObj->assign('message', 'Comment <em>'.$title.'</em> saved');
         }
     }
     return $rval;
@@ -124,18 +136,18 @@ function saveEdit(&$bBlog){
 function retrieveComments(&$bBlog, $amount){
     if ((isset($_POST['post_comments'])) && (is_numeric($_POST['post_comments']))) {
         $post_comments_q = "SELECT * FROM `".T_COMMENTS."` , `".T_POSTS."` WHERE `".T_POSTS."`.`postid`=`".T_COMMENTS."`.`postid` and deleted='false' and `".T_COMMENTS."`.`postid`='".$_POST['post_comments']."' order by `".T_COMMENTS."`.`posttime` desc";
-        $bBlog->assign('comments',$bBlog->get_results($post_comments_q));
-        $bBlog->assign('message','Showing comments for PostID '.$_POST['post_comments']); //.'.<br /><a href="index.php?b=plugins&amp;p=comments">Click here to show 50 most recent comments</a>.');
+        $bBlog->smartyObj->assign('comments',$bBlog->get_results($post_comments_q));
+        $bBlog->smartyObj->assign('message','Showing comments for PostID '.$_POST['post_comments']); //.'.<br /><a href="index.php?b=plugins&amp;p=comments">Click here to show 50 most recent comments</a>.');
     } else {
-        //$bBlog->assign('message','Showing '.$amount.' most recent comments across all posts. ');
-        $bBlog->assign('comments',$bBlog->get_results("SELECT * FROM `".T_COMMENTS."` , `".T_POSTS."` WHERE `".T_POSTS."`.`postid`=`".T_COMMENTS."`.`postid` and deleted='false' order by `".T_COMMENTS."`.`posttime` desc limit 0,".$amount));
-        $bBlog->assign('commentAmount', $amount);
+        //$bBlog->smartyObj->assign('message','Showing '.$amount.' most recent comments across all posts. ');
+        $bBlog->smartyObj->assign('comments',$bBlog->get_results("SELECT * FROM `".T_COMMENTS."` , `".T_POSTS."` WHERE `".T_POSTS."`.`postid`=`".T_COMMENTS."`.`postid` and deleted='false' order by `".T_COMMENTS."`.`posttime` desc limit 0,".$amount));
+        $bBlog->smartyObj->assign('commentAmount', $amount);
     }
 }
 
 function populateSelectList(&$bBlog){
     $posts_with_comments_q = "SELECT ".T_POSTS.".postid, ".T_POSTS.".title, count(*) c FROM ".T_COMMENTS.",  ".T_POSTS." 	WHERE ".T_POSTS.".postid = ".T_COMMENTS.".postid GROUP BY ".T_POSTS.".postid ORDER BY ".T_POSTS.".posttime DESC ";
     $posts_with_comments = $bBlog->get_results($posts_with_comments_q,ARRAY_A);
-    $bBlog->assign("postselect",$posts_with_comments);
+    $bBlog->smartyObj->assign("postselect",$posts_with_comments);
 }
 ?>

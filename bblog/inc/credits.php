2e1161bb71c92f4289b73ce4382edc005bd4be84
xushi <xushi@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-08-05 15:39:52 +0000

    Credits include us active admins/devs. Link to wiki for all the previous ones.
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@64 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/inc/credits.php
+++ bblog/inc/credits.php
@@ -1,70 +1,25 @@
 <?php
 // make it an array for more flexable layout
 $credits = array(
-'bBlog core code / project founder  <a href="mailto:email@eadz.co.nz">Eaden McKee</a> - <a href="http://www.eadz.co.nz/blog/" target="_blank">eadz.co.nz/blog</a>',
-
-"Archives funtions and various other various bits by <a href='http://www.revjim.net/' target='_blank'>Rev Jim</a>, core dev team",
-
-"<a href='http://www.trendwhores.de/'>Tobias Schlottke</a> - core dev team",
-
-"<a href='http://www.lwo-lab.net/'>Elie Bleton</a> - core dev team",
-
-"Better recentposts plugin and admin interface design among other things by <a href='http://www.sebastian-werner.net/' target='_blank'>Sebastian Werner</a>, core dev team",
-
-"<a href='http://www.toolmantim.com/' target='_blank'>Tim Lucas</a> for actually knowing regex and making a wicked readmore plugin, among other things, core dev team",
-
-"Messju Mohr (Smarty Author), helping out with smarty issues",
-
-"Database class (ez_sql) by  Justin Vincent",
-
-"Smarty code by the smart folks at <a href='http://smarty.php.net/' target='_blank'>smarty.php.net</a>",
-
-"Mario Delgado - Links Plugin",
-
-"<a href='http://www.keithdevens.com/software/xmlrpc/'>Keith Devens - XML-RPC</a>",
-
-"<a href='mailto:markon@air4web.com'>Martin KonÃ­Ä?ek</a> - RSS Fetcher Plugin, WYSIWYG editor plug-in, Diagnostic plug-in, Fancy URL plug-in, Image resizer plug-in, Secure Authentication",
-
-"<a href='http://sourceforge.net/projects/tinymce/'>Johan Spocke SÃ¶rlin</a> - TinyMCE WYSIWYG editor",
-
-"<a href='mailto:kellan@protest.net'>Kellan Elliott-McCrea</a> - <a href='http://magpierss.sourceforge.net' target='_blank'>MagpieRSS</a>, used in RSS Fetcher Plugin",
-
-"textile and the since() function  by <a href='http://www.textism.com/' target='_blank'>Dean Allen</a>",
-
-"blogroll plugin based on code from <a href='http://www.philringnalda.com/phpblogroll/' target='_blank'>Phil Ringnalda's blogroll</a>",
-
-"<a href='http://glutnix.webfroot.co.nz/'>Brett Taylor</a> - Feedback and beta testing",
-
-"Tanel Raja - New calendar plugin",
-
-"Ulf Harnhammar - <a href='http://sf.net/projects/kses/' target='_blank'>KSES</a> - HTML/XHTML filter that only allows some elements and attributes",
-
-"Some http header and bookmarklet code from b2/<a href='http://www.wordpress.org/' target='_blank'>wordpress</a>",
-
-
-"bbcode smarty modifier by Andre Rabold , with make_clickable by Nathan Codding
-and documentation from <a href='http://www.phpbb.com/' target='_blank'>PHPBB</a>",
-
-"Admin post hidden options thingy and readmore plugin inspired by the one on <a  href='http://www.livejournal.com/' target='_blank'>LiveJournal</a>",
-
-"Calendar Javascript inspired by the calendar that comes with pivotlog",
-
-"Lines theme from from <a href='http://www.movablestyle.com/'>Movable Style</a>, Released under the GPL by <a href='http://scott.yang.id.au/'>Scott Yang</a>",
-
-"MT API support, <a href='http://www.bluewire.net.nz/'>Mark Rowe</a>",
-
-"Icons from <a href='http://gtmcknight.com/buttons/index.php' target='_blank'>Steal These Buttons</a>",
-"Thomas Reynolds <thomasr@infograph.com>, atom fixes",
-"Wouter Wolkers <wouter@wolkers.nl> User Admin Interface",
-"Tiran Kenja <a href='http://www.codedivision.com'>codedivision.com</a> - Big patch for cleaner OO",
-"SomethingClever - <a href='http://blog.johnnaked.com/'>blog.johnnaked.com</a>",
-"Chris Boulton - <a href='http://www.surfionline.com/'>www.surfionline.com</a>",
-"Eric Y. Theriault - <a href='http://www.eyt.ca/'>www.eyt.ca</a>",
-"Michael Heilemann - <a href='http://binarybonsai.com/kubrick/'>kubrick</a>",
-"Raefer Gabriel - <a href='http://hardgrok.org/'>hardgrok.org</a>",
-"<a href='http://javaducky.com/'>Paul Balogh</a> - <i>External Content</i> plugin, post preview and miscellaneous fixes",
-"<a href='http://www.toddcharron.com/'>Todd Charron</a> - installation updates, bug fixes, release coordinating",
-"Anyone else who has helped out on the bBlog wiki or forums. If you've contributed code and you're not on this page <a href='mailto:eadz@webforce.co.nz'>Email me!</a>"
+"				---------- ----------",
+"				    The Admin Team",
+"				---------- ----------",
+'<a href="mailto:email@eadz.co.nz">Eadz</a> - <a href="http://www.eadz.co.nz" target=_blank>Eaden McKee</a>, bBlog core code / project founder.',
+"<a href='mailto:asdf@asdf.com'>Pixelpope</a> - <a href='http://pixelpope.com' target=_blank>Dominic Frohlof</a> - Multilingual & RTL/LTR support, bug fixing, features, testing.",
+"<a href='mailto:asdf@asdf.com'>Hijacker</a> - <a href='http://sjuengling.xetronet.de/' target=_blank>Sascha Jüngling</a> - Organisatorics, documentation",
+"<a href='mailto:xushi.xushi@gmail.com'>Xushi</a> - <a href='http://xushi.co.uk' target=_blank>Samir Greadly</a> - Bug fixes, added features, testing, upgrade code enhancement",
+"<a href='kenneth.power@gmail.com'>Telcor</a> - <a href='http://blog.tel-cor.com' target=_blank>Kenneth Power</a> - Coding, bug fixes, testing",
+"<a href='mailto:asdf@asdf.com'>Clever</a> - <a href='http://blog.johnnaked.com' target=_blank>Edbury Raymond Enegren IV</a> - Testing, plugin implementation",
+"<a href='mailto:asdf@asdf.com'>Woe</a> - <a href='http://woe-labs.net' target=_blank>Elie Bleton</a> - core programming and plugin hacking.",
+"",
+"				---------- ----------",
+"				    The Dev Team",
+"				---------- ----------",
+"<a href='mailto:asdf@asdf.com'>Wormeyman</a> - <a href='http://www.wormeyman.com/' target=_blank>Eric Johnson</a> - Coding, bug fixing, plugins",
+"<a href='mailto:asdf@asdf.com'>DL8</a> - <a href='http://webbi.cheetux.org.il' target=_blank>Nir Lavi</a> - Bug fixes, testing.",
+"<a href='mailto:asdf@asdf.com'>Kleppa</a> - ",
+"<a href='mailto:asdf@asdf.com'>Toby</a> - ",
+"<a href='http://www.bblog.com/wiki/index.php/Authors' target=_blank>And the rest of the dev team and contributors</a>",
 );
 ?>
 

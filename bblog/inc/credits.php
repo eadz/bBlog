fed08b2bf812ee116ba83efbdedcf986985e67a6
xushi <xushi@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-08-06 21:16:25 +0000

    * (finally) remembered Klepp's name :p
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@73 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/inc/credits.php
+++ bblog/inc/credits.php
@@ -1,25 +1,26 @@
 <?php
+
 // make it an array for more flexable layout
 $credits = array(
 "				---------- ----------",
-"				    The Admin Team",
+"				 The Admin / Dev Team",
 "				---------- ----------",
 '<a href="mailto:email@eadz.co.nz">Eadz</a> - <a href="http://www.eadz.co.nz" target=_blank>Eaden McKee</a>, bBlog core code / project founder.',
-"<a href='mailto:asdf@asdf.com'>Pixelpope</a> - <a href='http://pixelpope.com' target=_blank>Dominic Frohlof</a> - Multilingual & RTL/LTR support, bug fixing, features, testing.",
-"<a href='mailto:asdf@asdf.com'>Hijacker</a> - <a href='http://sjuengling.xetronet.de/' target=_blank>Sascha Jüngling</a> - Organisatorics, documentation",
-"<a href='mailto:xushi.xushi@gmail.com'>Xushi</a> - <a href='http://xushi.co.uk' target=_blank>Samir Greadly</a> - Bug fixes, added features, testing, upgrade code enhancement",
-"<a href='kenneth.power@gmail.com'>Telcor</a> - <a href='http://blog.tel-cor.com' target=_blank>Kenneth Power</a> - Coding, bug fixes, testing",
-"<a href='mailto:asdf@asdf.com'>Clever</a> - <a href='http://blog.johnnaked.com' target=_blank>Edbury Raymond Enegren IV</a> - Testing, plugin implementation",
-"<a href='mailto:asdf@asdf.com'>Woe</a> - <a href='http://woe-labs.net' target=_blank>Elie Bleton</a> - core programming and plugin hacking.",
+"Pixelpope - <a href='http://pixelpope.com' target=_blank>Dominic Frohlof</a> - Multilingual & RTL/LTR support, bug fixing, features, testing.",
+"Hijacker - <a href='http://sjuengling.xetronet.de/' target=_blank>Sascha Jüngling</a> - Organisatorics, documentation",
+"Xushi - <a href='http://xushi.co.uk' target=_blank>Samir Greadly</a> - Bug fixes, added features, testing, upgrade code enhancement",
+"Telcor - <a href='http://blog.tel-cor.com' target=_blank>Kenneth Power</a> - Coding, bug fixes, testing",
+"Clever - <a href='http://blog.johnnaked.com' target=_blank>Edbury Raymond Enegren IV</a> - Testing, plugin implementation",
+"Woe - <a href='http://woe-labs.net' target=_blank>Elie Bleton</a> - core programming and plugin hacking.",
 "",
 "				---------- ----------",
 "				    The Dev Team",
 "				---------- ----------",
-"<a href='mailto:asdf@asdf.com'>Wormeyman</a> - <a href='http://www.wormeyman.com/' target=_blank>Eric Johnson</a> - Coding, bug fixing, plugins",
-"<a href='mailto:asdf@asdf.com'>DL8</a> - <a href='http://webbi.cheetux.org.il' target=_blank>Nir Lavi</a> - Bug fixes, testing.",
-"<a href='mailto:asdf@asdf.com'>Kleppa</a> - ",
-"<a href='mailto:asdf@asdf.com'>Toby</a> - ",
-"<a href='http://www.bblog.com/wiki/index.php/Authors' target=_blank>And the rest of the dev team and contributors</a>",
+"Wormeyman - <a href='http://www.wormeyman.com/' target=_blank>Eric Johnson</a> - Coding, bug fixing, plugins",
+"DL8 - <a href='http://webbi.cheetux.org.il' target=_blank>Nir Lavi</a> - Bug fixes, testing.",
+"Kleppa - <a href='http://www.asdf.com' target=_blank>Anders Klepaker</a> - Bug fixing, plugin development",
+"Tobius - <a href='http://www.tobymiller.com/' target=_blank>Toby Miller</a> - Bug fixing, plugin development",
+"<a href='http://www.bblog.com/wiki/index.php/Authors' target=_blank>And the rest of the dev team and contributors</a>"
 );
 ?>
 

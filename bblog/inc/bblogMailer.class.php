a5dfa3870a2dfb87ee3b28fb814cbead279e4e4c
xushi <xushi@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-08-07 13:47:37 +0000

    * let bblog choose localhost as smtp server first before something else.
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@76 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/inc/bblogMailer.class.php
+++ bblog/inc/bblogMailer.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * bblogMailer.class.php
+ * bblogMailer.class.php - extends the PHPMailer class
  * <p>
  * Here's a class that extends the PHPMailer class 
  * and sets the defaults for our bBlog site.
@@ -9,7 +9,7 @@
  * <p>
  * @author xushi - <xushi.xushi@gmail.com>
  * @source phpmailer - <http://phpmailer.sourceforge.net/>
- * @licence GPL
+ * @licence GPL <http://www.gnu.org/copyleft/gpl.html>
  */
 
 require("class.phpmailer.php");
@@ -18,21 +18,22 @@ class bblogMailer extends PHPMailer {
 
     // Set default variables for all new objects
     
-    // TODO: i'd rather have the $Host be read from
-    // the db instead of writing it here. Just incase
-    // any robot would come, or any faggot wants to 
-    // hack this file for spam reasons.
+    // TODO: i'd rather have $Host be read from bb_config,
+    // instead of writing it here. Just incase any robot 
+    // would come, or any faggot wants to hack this file 
+    // for spam reasons. Same goes with $From, to be read
+    // from bb_config (the email there.)
     var $From     = "bblog@bblog.com";
     var $FromName = "The bBlog Team";
-    var $Host     = "xushi.co.uk;xushi.co.uk"; //currently mine.. change to bblog's later.
+    var $Host     = "localhost;xushi.co.uk"; //currently mine.. change to bblog's or alternative later.
     var $Mailer   = "smtp";                         // Alternative to IsSMTP()
     var $WordWrap = 75;
 
     
     // Replace the default error_handler
     function error_handler($msg) {
-        print("My Site Error");
-        print("Description:");
+        print("bBlog Mailer Error");
+        print("Description: ... err, something wrong happened...");
         printf("%s", $msg);
         exit;
     }

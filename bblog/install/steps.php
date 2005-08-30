d29fe8c49110958f53b05defc570b194bd97771e
tobymiller <tobymiller@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-08-30 21:46:47 +0000

    added a variable check for $EZSQL_ERROR to remove a php warning
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@99 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/install/steps.php
+++ bblog/install/steps.php
@@ -8,41 +8,41 @@
  * @copyright The bBlog Project, http://www.bblog.com/
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
  */
- 
+
 // can you get to level 6?
 switch($step) {
-	case 0:
-		// tests to get to next level :
-		// 1 - agree to terms
-		if ((!isset($_POST['agree']) || ($_POST['agree'] != 'yes'))) {
-			if ((isset($_POST['submit'])) && ($_POST['submit'])) $message = "<p style='color:red;'>You must agree to the terms</p>";
-			break;
-		} 
-		
-		if (isset($_POST['install_type'])) {
-			$config['install_type'] = $_POST['install_type'];
-			
-			if ($_POST['install_type'] == 'upgrade') {
-				$config['upgrade_from'] = $_POST['upgrade_from'];
-			}
-		} else break;
-				
-		$step=1;
-		break;
-		
-	case 1:
-		// tests to get to next level
-		// 1 - things need to be writable
-		ob_start(); // we don't want any errors
-		if(check_writable()) $step = 2;
-		ob_end_clean();
-		break;
-	case 2:
-		// tests : 
-		// 1 - mysql connects
-		// 2 - everything is set
-		$missing_fields = '';
-		$config_vals = array(
+    case 0:
+        // tests to get to next level :
+        // 1 - agree to terms
+        if ((!isset($_POST['agree']) || ($_POST['agree'] != 'yes'))) {
+            if ((isset($_POST['submit'])) && ($_POST['submit'])) $message = "<p style='color:red;'>You must agree to the terms</p>";
+            break;
+        }
+
+        if (isset($_POST['install_type'])) {
+            $config['install_type'] = $_POST['install_type'];
+
+            if ($_POST['install_type'] == 'upgrade') {
+                $config['upgrade_from'] = $_POST['upgrade_from'];
+            }
+        } else break;
+
+        $step=1;
+        break;
+
+    case 1:
+        // tests to get to next level
+        // 1 - things need to be writable
+        ob_start(); // we don't want any errors
+        if(check_writable()) $step = 2;
+        ob_end_clean();
+        break;
+    case 2:
+        // tests :
+        // 1 - mysql connects
+        // 2 - everything is set
+        $missing_fields = '';
+        $config_vals = array(
             'blogname'=>'Blog name',
             'blogdescription' => 'Blog description',
             'username' => 'Username',
@@ -58,47 +58,47 @@ switch($step) {
             'mysql_database' => 'MySQL Database',
             'mysql_host' => 'MySQL Host',
             'table_prefix' => 'MySQL Table prefix'
-		);
-		
-		$allfilled=TRUE;
-		foreach($config_vals as $field=>$prompt){
+        );
+
+        $allfilled=TRUE;
+        foreach($config_vals as $field=>$prompt){
             if(isset($_POST[$field]) && !empty($_POST[$field]))
                 $config[$field] = $_POST[$field];
             else
                 $missing_fields .= $prompt.' ';
-		}
-		
-		//Test first to see if the passwords both match
-		if ($config['password'] !== $config['secondPassword']){
-			$missing_fields .= "Passwords mismatched.";
-		}
-		//trim bblog/install/index.php off the URL
-		$pos = strpos($config['url'], 'bblog/install/');
-        $config['url'] = ($pos !== false) ? substr($config['url'], 0, $pos) : $config['url'];
-            
-		if (strlen($missing_fields) > 0) {
-			$message = "<p style='color:red;'>You must fill all the fields. Following fields are missing<br />$missing_fields</p>";
-			break;
-		}
+        }
 
-		// try to connect to db
-		$db = new db($config['mysql_username'],$config['mysql_password'],$config['mysql_database'],$config['mysql_host']);
-		
-		/**
-		 * xushi: i get an notice warning in the logs when installing regarding this line
-		 * PHP Stack trace:, referer: bblog/install/index.php
-		 * PHP   1. {main}() bblog/install/index.php:0, referer: http://localhost/xushi/08/bblog/install/index.php
-		 * PHP   2. include() bblog/install/index.php:121, referer: http://localhost/xushi/08/bblog/install/index.php
-		 * PHP Notice:  Undefined variable: EZSQL_ERROR in bblog/install/steps.php on line 131, referer: http://localhost/xushi/08/bblog/install/index.php
-		 */				 
-		if(is_array($EZSQL_ERROR)) {
-			$message = (isset($EZSQL_ERROR[0]['error_str']) ? $EZSQL_ERROR[0]['error_str'] : '');
-			
-			break;
-		}
-		
-		$step = 4;
-		$_SESSION['config'] =& $config;
-		break;
+        //Test first to see if the passwords both match
+        if ($config['password'] !== $config['secondPassword']){
+            $missing_fields .= "Passwords mismatched.";
+        }
+        //trim bblog/install/index.php off the URL
+        $pos = strpos($config['url'], 'bblog/install/');
+        $config['url'] = ($pos !== false) ? substr($config['url'], 0, $pos) : $config['url'];
+
+        if (strlen($missing_fields) > 0) {
+            $message = "<p style='color:red;'>You must fill all the fields. Following fields are missing<br />$missing_fields</p>";
+            break;
+        }
+
+        // try to connect to db
+        $db = new db($config['mysql_username'],$config['mysql_password'],$config['mysql_database'],$config['mysql_host']);
+
+        /**
+         * xushi: i get an notice warning in the logs when installing regarding this line
+         * PHP Stack trace:, referer: bblog/install/index.php
+         * PHP   1. {main}() bblog/install/index.php:0, referer: http://localhost/xushi/08/bblog/install/index.php
+         * PHP   2. include() bblog/install/index.php:121, referer: http://localhost/xushi/08/bblog/install/index.php
+         * PHP Notice:  Undefined variable: EZSQL_ERROR in bblog/install/steps.php on line 131, referer: http://localhost/xushi/08/bblog/install/index.php
+         */
+         if (isset($EZSQL_ERROR) && is_array($EZSQL_ERROR))
+         {
+            $message = $EZSQL_ERROR[0]['error_str'];
+            break;
+        }
+
+        $step = 4;
+        $_SESSION['config'] =& $config;
+        break;
 }
 ?>

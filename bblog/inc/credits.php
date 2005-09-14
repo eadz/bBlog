db099b177ce1845c9b0ac496f32712e185ed483f
hijacker <hijacker@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-09-14 21:38:43 +0000

    very minor update in credits.php
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@118 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/inc/credits.php
+++ bblog/inc/credits.php
@@ -7,7 +7,7 @@ $credits = array(
 "				---------- ----------",
 '<a href="mailto:email@eadz.co.nz">Eadz</a> - <a href="http://www.eadz.co.nz" target=_blank>Eaden McKee</a>, bBlog core code / project founder.',
 "Pixelpope - <a href='http://pixelpope.com' target=_blank>Dominic Frohlof</a> - Multilingual & RTL/LTR support, bug fixing, features, testing.",
-"Hijacker - <a href='http://sjuengling.xetronet.de/' target=_blank>Sascha Jüngling</a> - Organisatorics, documentation",
+"Hijacker - <a href='http://sjuengling.xetronet.de/' target=_blank>Sascha J?ngling</a> - Organisatorics, documentation",
 "Xushi - <a href='http://xushi.co.uk' target=_blank>Samir Greadly</a> - Bug fixes, added features, testing, upgrade code enhancement",
 "Telcor - <a href='http://blog.tel-cor.com' target=_blank>Kenneth Power</a> - Coding, bug fixes, testing",
 "Clever - <a href='http://blog.johnnaked.com' target=_blank>Edbury Raymond Enegren IV</a> - Testing, plugin implementation",
@@ -16,11 +16,10 @@ $credits = array(
 "				---------- ----------",
 "				    The Dev Team",
 "				---------- ----------",
-"Wormeyman - <a href='http://www.wormeyman.com/' target=_blank>Eric Johnson</a> - Coding, bug fixing, plugins",
+"Wormeyman - <a href='http://www.wormeyman.com/' target=_blank>Eric Johnson</a> - Bug hunting, wiki/forum maintenance",
 "DL8 - <a href='http://webbi.cheetux.org.il' target=_blank>Nir Lavi</a> - Bug fixes, testing.",
 "Kleppa - <a href='http://www.asdf.com' target=_blank>Anders Klepaker</a> - Bug fixing, plugin development",
 "Tobius - <a href='http://www.tobymiller.com/' target=_blank>Toby Miller</a> - Bug fixing, plugin development",
 "<a href='http://www.bblog.com/wiki/index.php/Authors' target=_blank>And the rest of the dev team and contributors</a>"
 );
 ?>
-

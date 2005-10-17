7df7ba826d58c8751c82f65f775321937e5f633c
telcor <telcor@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-10-17 17:45:40 +0000

    Includes a backport of a PHP 5 function: array_diff_key
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@161 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/inc/functions.php
+++ bblog/inc/functions.php
@@ -14,18 +14,21 @@
 
 // !Pings weblogs.com, blo.gs, and others in the future
 // this is in it's own function so we can use register_shutdown_function
-function ping() {
-
+/**
+ * Pings external websites specified in the CONFIG table
+ * 
+ * This is registered as a shutdown function
+ */
+function ping(){
     $weblogsparams[0] = C_BLOGNAME;
     $weblogsparams[1] = BLOGURL;
     $sites = explode(',',C_PING);
     if(count($sites) > 0) {
-	foreach($sites as $site) {
-		$url = explode('/',$site);
-		XMLRPC_request($url[0], "/".$url[1], "weblogUpdates.ping", $weblogsparams, WEBLOG_XMLRPC_USERAGENT );
-	}
+    	foreach($sites as $site) {
+    		$url = explode('/',$site);
+    		XMLRPC_request($url[0], "/".$url[1], "weblogUpdates.ping", $weblogsparams, WEBLOG_XMLRPC_USERAGENT );
+    	}
     }
-
 }
 
 
@@ -73,4 +76,42 @@ function update_when_compiled($tpl_source, &$smartyObj) {
 function smarty_block_dynamic($param, $content, &$smarty) {
     return $content;
 }
+if (!function_exists('array_diff_key')){
+    /**
+    * Computes the difference of arrays using keys for comparison
+    * 
+    * @param array $valuesBase Base elements for comparison, associative
+    * @param array $valuesComp[,..] Comparison elements, associative
+    * 
+    * @param array Elements, not existing in comparison element, associative
+    */
+    function array_diff_key(){
+        $argCount   = func_num_args();
+        $argValues  = func_get_args();
+        $valuesDiff = array();
+        
+        if ($argCount < 2){
+            return false;
+        }
+        
+        foreach ($argValues as $argParam){
+            if (!is_array($argParam)){
+                return false;
+            }
+        }
+        
+        foreach ($argValues[0] as $valueKey => $valueData){
+        for ($i = 1; $i < $argCount; $i++){
+            if (isset($argValues[$i][$valueKey])){
+                continue 2;
+            }
+        }
+        
+            $valuesDiff[$valueKey] = $valueData;
+        }
+        
+        return $valuesDiff;
+    }
+}
+
 ?>

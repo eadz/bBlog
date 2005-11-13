2e18f1c77b263143440a17a2b35ec6ba692ce9f3
xushi <xushi@b1f881d5-fbf8-0310-bb95-8f3af07a723f>
2005-11-13 02:07:05 +0000

    fixed team list and removed pixel from admin
    git-svn-id: https://svn.code.sf.net/p/bblog/code/branches/0.8-cvs@167 b1f881d5-fbf8-0310-bb95-8f3af07a723f

--- bblog/inc/credits.php
+++ bblog/inc/credits.php
@@ -16,12 +16,10 @@ $credits = array(
 "				 The Admin / Dev Team",
 "				---------- ----------",
 'Eadz - <a href="http://www.eadz.co.nz/" target=_blank>Eaden McKee</a>, bBlog core code / project founder.',
-"Pixelpope - <a href='http://pixelpope.com' target=_blank>Dominic Frohlof</a> - Multilingual & RTL/LTR support, bug fixing, features, testing.",
 "Hijacker - <a href='http://sjuengling.xetronet.de/' target=_blank>Sascha J?ngling</a> - Project Coordinator, documentation",
 "Xushi - <a href='http://xushi.co.uk' target=_blank>Samir Greadly</a> - Project Admin, Forum maintanance, Bug fixes, added features, testing, code enhancement",
 "Telcor - <a href='http://blog.tel-cor.com' target=_blank>Kenneth Power</a> - Coding, bug fixes, testing",
-"Clever - <a href='http://blog.johnnaked.com' target=_blank>Edbury Raymond Enegren IV</a> - Testing, plugin implementation",
-"Woe - <a href='http://woe-labs.net' target=_blank>Elie Bleton</a> - core programming and plugin hacking.",
+"Clever - <a href='http://blog.johnnaked.com' target=_blank>Edbury Raymond Enegren IV</a> - Project Support, Testing, plugin implementation and hacking",
 "",
 "				---------- ----------",
 "				    The Dev Team",
@@ -30,6 +28,7 @@ $credits = array(
 "Javaducky - <a href='http://www.javaducky.com' target=_blank>Paul Balogh</a> - Plugins and features.",
 "Kleppa - <a href='http://www.asdf.com' target=_blank>Anders Klepaker</a> - Bug fixing, plugin development",
 "Tobius - <a href='http://www.tobymiller.com/' target=_blank>Toby Miller</a> - Bug fixing, plugin development",
+"Woe - <a href='http://woe-labs.net' target=_blank>Elie Bleton</a> - core programming and plugin hacking.",
 "<a href='http://www.bblog.com/wiki/index.php/Authors' target=_blank>And the rest of the dev team and contributors</a>"
 );
 ?>
